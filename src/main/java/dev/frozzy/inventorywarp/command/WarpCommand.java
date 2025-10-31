fa61947307fc80a9c8cc418425d1de9a4017b308
Elias Lauterbach <me@elias-lauterbach.de>
2025-10-31 20:07:50 +0100

    Implement basic command structure for WarpCommand with add/remove placeholders

--- src/main/java/dev/frozzy/inventorywarp/command/WarpCommand.java
+++ src/main/java/dev/frozzy/inventorywarp/command/WarpCommand.java
@@ -3,11 +3,29 @@ package dev.frozzy.inventorywarp.command;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
 
 public class WarpCommand implements CommandExecutor {
+
+    //warps add "Name" <Icon>
+    //warps
+    //warps remove "Name"
     @Override
-    public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String s, @NotNull String @NotNull [] strings) {
+    public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String @NotNull [] args) {
+        if (!(sender instanceof Player))
+            return false;
+
+        if (args.length == 2) {
+            switch (args[0]) {
+                case "add":
+
+                    break;
+                case "remove":
+                    break;
+            }
+        }
+
         return false;
     }
 }
