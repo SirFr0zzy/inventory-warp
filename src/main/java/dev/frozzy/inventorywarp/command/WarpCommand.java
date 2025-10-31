a14c0a73876e4ed8da8bfd0ec326e49a94748811
Elias Lauterbach <me@elias-lauterbach.de>
2025-10-31 19:39:50 +0100

    Initial Commit

---
+++ src/main/java/dev/frozzy/inventorywarp/command/WarpCommand.java
@@ -0,0 +1,13 @@
+package dev.frozzy.inventorywarp.command;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.jetbrains.annotations.NotNull;
+
+public class WarpCommand implements CommandExecutor {
+    @Override
+    public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String s, @NotNull String @NotNull [] strings) {
+        return false;
+    }
+}
