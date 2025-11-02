3a4b8188ba7dddf471d92ef2ad4e9d40da533869
Elias Lauterbach <me@elias-lauterbach.de>
2025-11-02 14:48:05 +0100

    feat: add id field to Warp record for unique identification

--- src/main/java/dev/frozzy/inventorywarp/util/Warp.java
+++ src/main/java/dev/frozzy/inventorywarp/util/Warp.java
@@ -6,6 +6,6 @@ import org.bukkit.Location;
 import org.bukkit.Material;
 
 
-public record Warp(String name, Location location, Material material) {
+public record Warp(Integer id, String name, Location location, Material material) {
 
 }
